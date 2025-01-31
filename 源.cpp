f144527c0638c3ca35ce5c3ed14c46824926fa32
Universe-hy <3260372408@qq.com>
2025-01-31 18:29:27 +0800

    AcWing 901. 滑雪  AC

--- 源.cpp
+++ 源.cpp
@@ -11,24 +11,27 @@
 #include<queue>
 using namespace std;
 const int N = 310;
-int v[N][N],f[N][N],rs[N][N];
+int v[N][N], f[N][N], rs[N][N];
 int r, c;
-int dfs(int a,int b) {
+int dfs(int a, int b) {
+	if (rs[a][b])
+		return rs[a][b];
+
 	if (a - 1 >= 0 && v[a - 1][b] < v[a][b])
-		rs[a][b] = max(rs[a][b], rs[a - 1][b] ? rs[a - 1][b] : dfs(a - 1, b));
+		rs[a][b] = max(rs[a][b], dfs(a - 1, b));
 	if (b - 1 >= 0 && v[a][b - 1] < v[a][b])
-		rs[a][b] = max(rs[a][b], rs[a][b - 1] ? rs[a][b - 1] : dfs(a, b - 1));
+		rs[a][b] = max(rs[a][b], dfs(a, b - 1));
 	if (a + 1 < r && v[a + 1][b] < v[a][b])
-		rs[a][b] = max(rs[a][b], rs[a + 1][b] ? rs[a + 1][b] : dfs(a + 1, b));
+		rs[a][b] = max(rs[a][b], dfs(a + 1, b));
 	if (b + 1 < c && v[a][b + 1] < v[a][b])
-		rs[a][b] = max(rs[a][b], rs[a][b + 1] ? rs[a][b + 1] : dfs(a, b + 1));
+		rs[a][b] = max(rs[a][b], dfs(a, b + 1));
 	rs[a][b]++;
 	return rs[a][b];
 }
 
 int main() {
-	scanf("%d %d", &r,&c);
-	for (int i = 0; i <r; i++)
+	scanf("%d %d", &r, &c);
+	for (int i = 0; i < r; i++)
 		for (int q = 0; q < c; q++)
 		{
 			scanf("%d", &v[i][q]);
@@ -36,7 +39,8 @@ int main() {
 	int ans = 0;
 	for (int i = 0; i < r; i++)
 		for (int q = 0; q < c; q++)
-			ans=max(ans, rs[i][q] ? rs[i][q] : dfs(i, q));	
+			ans = max(ans, dfs(i, q));
+	cout << ans;
+
 	return 0;
 }
-
