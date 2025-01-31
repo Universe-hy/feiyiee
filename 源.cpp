1d08447fd9000cfbf98917526d038dcdd594c0bb
Universe-hy <3260372408@qq.com>
2025-01-31 10:34:01 +0800

    AcWing 285. 没有上司的舞会  AC

--- 源.cpp
+++ 源.cpp
@@ -10,47 +10,4 @@
 #include<map>
 using namespace std;
 
-long long int f[12][1<<12];
-int n, m;
-int cheak(int a) {
-	int fig = 0;
-	for (int i = 0; i < m; i++) {
-		if (a & 1) {
-			if (fig & 1)return 0;
-			fig = 0;
-		}else fig++;
-		a >>= 1;
-	}
-	return !(fig & 1);
-}
 
-int main() {
-	
-
-	while(cin>>n>>m,n||m)
-	{
-		if (n == 1) {
-			cout << !(m &1 ) << endl;
-		}
-		else {
-			for (int i = 0; i < (1 << m); i++)
-				f[1][i]= cheak(i);
-
-			for (int i = 2; i < n; i++) {
-				for (int q = 0; q < (1 << m); q++) {
-					f[i][q] = 0;
-					for (int w = 0; w < (1 << m); w++) {
-						if (!(q & w) && f[1][q | w]) {
-							f[i][q] += f[i - 1][w];
-						}
-					}
-				}
-			}
-			long long int ans = 0;
-			for (int w = 0; w < (1 << m); w++)
-				if(w&& f[1][w])
-					ans += f[n - 1][w];
-			cout << ans << endl;
-		}
-	}
-}
