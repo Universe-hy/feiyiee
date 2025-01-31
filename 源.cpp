ca97406419906fa82f4124c51885c9578660925b
Universe-hy <3260372408@qq.com>
2025-01-31 19:19:34 +0800

    AcWing 125. 耍杂技的牛 AC

--- 源.cpp
+++ 源.cpp
@@ -5,6 +5,7 @@
 
 #include <windows.h>
 #include<iostream>
+#include<algorithm>
 #include <stdlib.h>
 #include<string>
 #include<map>
@@ -13,34 +14,26 @@ using namespace std;
 const int N = 310;
 int v[N][N], f[N][N], rs[N][N];
 int r, c;
-int dfs(int a, int b) {
-	if (rs[a][b])
-		return rs[a][b];
 
-	if (a - 1 >= 0 && v[a - 1][b] < v[a][b])
-		rs[a][b] = max(rs[a][b], dfs(a - 1, b));
-	if (b - 1 >= 0 && v[a][b - 1] < v[a][b])
-		rs[a][b] = max(rs[a][b], dfs(a, b - 1));
-	if (a + 1 < r && v[a + 1][b] < v[a][b])
-		rs[a][b] = max(rs[a][b], dfs(a + 1, b));
-	if (b + 1 < c && v[a][b + 1] < v[a][b])
-		rs[a][b] = max(rs[a][b], dfs(a, b + 1));
-	rs[a][b]++;
-	return rs[a][b];
+struct ni{
+	int w, s;
+}niu[50005];
+
+bool cmp(ni a,ni b) {
+	return a.s + a.w < b.s + b.w;
 }
 
 int main() {
-	scanf("%d %d", &r, &c);
-	for (int i = 0; i < r; i++)
-		for (int q = 0; q < c; q++)
-		{
-			scanf("%d", &v[i][q]);
-		}
-	int ans = 0;
-	for (int i = 0; i < r; i++)
-		for (int q = 0; q < c; q++)
-			ans = max(ans, dfs(i, q));
+	int n;
+	scanf("%d", &n);
+	for(int i=0;i<n;i++)
+		scanf("%d %d", &niu[i].w, &niu[i].s);
+	sort(niu, niu+n, cmp);
+	int tw = 0,ans= -1000000500;
+	for (int i = 0; i < n; i++) {
+		ans =max(ans, tw - niu[i].s);
+		tw += niu[i].w;
+	}
 	cout << ans;
-
 	return 0;
 }
