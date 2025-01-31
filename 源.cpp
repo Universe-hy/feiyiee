6fd847847f8a5b2c8ec8cd26c8287b8123fbbedd
Universe-hy <3260372408@qq.com>
2025-01-31 16:05:36 +0800

    AcWing 285. 没有上司的舞会 AC

--- 源.cpp
+++ 源.cpp
@@ -8,6 +8,37 @@
 #include <stdlib.h>
 #include<string>
 #include<map>
+#include<queue>
 using namespace std;
+const int N = 6010;
+int v[N],f[N],g[N],rs1[N],rs2[N];
+queue<int> hod;
 
+int main() {
+	int n, a, b;
+	scanf("%d", &n);
+	for (int i = 1; i <= n; i++)
+		scanf("%d", &v[i]);
+	for (int i = 1; i < n; i++) {
+		scanf("%d %d", &a, &b);
+		f[a] = b; g[b]++;
+	}
+
+	for (int i = 1; i <=n; i++)
+		if (!g[i]) hod.push(i);
+
+	while (1) {
+		a = hod.front();
+		rs1[a] += v[a];
+		if (f[a] == 0)break;
+		hod.pop();
+		rs1[f[a]] += rs2[a];
+		rs2[f[a]] += max(rs1[a],rs2[a]);
+		g[f[a]]--;
+		if(g[f[a]]==0)
+			hod.push(f[a]);
+	}
+	cout << max(rs1[hod.front()],rs2[hod.front()]);
+	return 0;
+}
 
