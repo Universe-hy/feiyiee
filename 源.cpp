a2f77eff584614b49391719adbe52daafb50e36a
Universe-hy <3260372408@qq.com>
2025-02-01 20:27:47 +0800

    试除法判定质数

--- 源.cpp
+++ 源.cpp
@@ -10,30 +10,30 @@
 #include<string>
 #include<map>
 #include<queue>
+
 using namespace std;
-const int N = 310;
-int v[N][N], f[N][N], rs[N][N];
-int r, c;
 
-struct ni{
-	int w, s;
-}niu[50005];
-
-bool cmp(ni a,ni b) {
-	return a.s + a.w < b.s + b.w;
+bool is_prime(int x)
+{
+    if (x < 2) return false;
+    for (int i = 2; i <= x / i; i++)
+        if (x % i == 0)
+            return false;
+    return true;
 }
 
-int main() {
-	int n;
-	scanf("%d", &n);
-	for(int i=0;i<n;i++)
-		scanf("%d %d", &niu[i].w, &niu[i].s);
-	sort(niu, niu+n, cmp);
-	int tw = 0,ans= -1000000500;
-	for (int i = 0; i < n; i++) {
-		ans =max(ans, tw - niu[i].s);
-		tw += niu[i].w;
-	}
-	cout << ans;
-	return 0;
+int main()
+{
+    int n;
+    cin >> n;
+
+    while (n--)
+    {
+        int x;
+        cin >> x;
+        if (is_prime(x)) puts("Yes");
+        else puts("No");
+    }
+
+    return 0;
 }
