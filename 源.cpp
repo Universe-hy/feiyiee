8491ce3424c0fc7a564ae82b89707f228d362a94
Universe-hy <3260372408@qq.com>
2025-02-01 20:29:43 +0800

    分解质因数

--- 源.cpp
+++ 源.cpp
@@ -12,27 +12,28 @@
 #include<queue>
 
 using namespace std;
-
-bool is_prime(int x)
+void divide(int x)
 {
-    if (x < 2) return false;
     for (int i = 2; i <= x / i; i++)
         if (x % i == 0)
-            return false;
-    return true;
+        {
+            int s = 0;
+            while (x % i == 0) x /= i, s++;
+            cout << i << ' ' << s << endl;
+        }
+    if (x > 1) cout << x << ' ' << 1 << endl;
+    cout << endl;
 }
 
 int main()
 {
     int n;
     cin >> n;
-
     while (n--)
     {
         int x;
         cin >> x;
-        if (is_prime(x)) puts("Yes");
-        else puts("No");
+        divide(x);
     }
 
     return 0;
