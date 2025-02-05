ff564774e079503c8e3e8c1f84918b7d5c4e75bf
Universe-hy <3260372408@qq.com>
2025-02-05 12:25:39 +0800

    AcWing 913. 排队打水 AC

--- 源.cpp
+++ 源.cpp
@@ -1,6 +1,4 @@
 #define _CRT_SECURE_NO_WARNINGS 1
-#define struct_size 10
-#define queue_size 5
 #pragma warning(disable:6031)
 
 #include <windows.h>
@@ -12,27 +10,24 @@
 #include<queue>
 
 using namespace std;
-const int N = 100010;
+const int N = 10010;
 int a[N];
 
 int main()
 {
     int n;
-    long long int dis = 0;
     cin >> n;
     int b;
     for (int i = 0; i < n; i++) {
         cin >> b;
-        dis += b;
         a[b]++;
     }
-    int r = a[0], l = n - a[0];
-    long long int ans = dis;
-    for (int i = 1; i < 40050; i++) {
-        dis += -l + r;
-        l -= a[i];
-        r += a[i];
-        ans = min(ans, dis);
+    int ans =0,sum=0;
+    for (int i = 1; i < 10010; i++) {
+        for (int q = 0; q < a[i]; q++) {
+            sum += i;
+            ans += sum;
+        }
     }
     cout << ans;
     return 0;
