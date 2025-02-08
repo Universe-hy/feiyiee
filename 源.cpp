f5502f7a702e602dcb7ae2d7da2f98bd17921f6c
Universe-hy <3260372408@qq.com>
2025-02-08 20:18:25 +0800

    AcWing 906. 区间分组  AC

--- 源.cpp
+++ 源.cpp
@@ -14,46 +14,23 @@ const int N = 10010;
 
 typedef pair<int, int> PII;
 vector<PII> a;
-
-
+priority_queue<int,vector<int>,greater<int> > group;
 int main()
 {
-    int s,t;
-    cin >> s>>t;
     int n;
     cin >> n;
-    for (int i = 1; i <= n; i++) {
+    for (int i = 0; i < n; i++) {
         int x, y;
         cin >> x >> y;
         a.push_back({ x, y });
     }
     sort(a.begin(), a.end());
 
-    int l = s;
-    int r = -1000000000;
-    int ans = 1;
     for (int i = 0; i < n; i++) {
-        if (a[i].first <= l) {
-            if (r < a[i].second)
-            {
-                r = a[i].second;
-                if(t<=r)
-                {
-                    cout << ans;
-                    return 0;
-                }
-            }
-        }
-        else if ( a[i].first<=r ) {
-            l = r;
-            ans++;
-            i--;
-        }
-        else {
-            cout << -1;
-            return 0;
-        }
+        if (group.size() && group.top() < a[i].first)
+            group.pop();
+        group.push(a[i].second);
     }
-    cout <<  -1;   
+    cout << group.size();
     return 0;
 }
