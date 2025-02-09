2572cd9c8649a40b72ea4ac82a9c6b75bba7d679
Universe-hy <3260372408@qq.com>
2025-02-09 19:01:52 +0800

    P11680 [Algo Beat Contest 001 B] Between Head and Tail 未完成

--- 源.cpp
+++ 源.cpp
@@ -11,29 +11,59 @@
 
 using namespace std;
 const int N = 100010;
+char a[N];
 
-typedef pair<int, int> PII;
-vector<PII> a;
-priority_queue<int,vector<int>,greater<int> > group;
+int wei(int x) {
+    int ans = 0;
+    while (x) {
+        x /= 10;
+        ans++;
+    }
+    return x;
+}
+
+int ten[10];
 int main()
 {
-    int n;
-    cin >> n;
-    for (int i = 0; i < n; i++) {
-        int x, y;
-        cin >> x >> y;
-        a.push_back({ x, y });
+    ten[0] = 1;
+    for (int i = 1; i < 10; i++) {
+        ten[i] = ten[i - 1] * 10;
     }
-    sort(a.begin(), a.end());
+    int n,m;
+    cin >> n;
+    int num = 0;
+    cin >> a;
+    int res = 0,r=0;
+    int k = 0;
+    for(int i=0;i<n;i++)
+    {
+        int l, r;
+        scanf("%d %d", &l, &r);
+        int er = wei(r);
+        for (int e = wei(l); e <=er ; e++) {
+            if(e>=3)
+                for (int q = 1; q < 10; q++)
+                {
+                    int rw = e;
+                    int qw = rw / 2;
+                    int kw = rw - 2;
+                    for (int k = 0; k < ten[kw]; k++) {
+                        int ka = k / ten[kw / 2];
+                        int kb = k % ten[kw / 2];
+                        res = q * ten[qw * 2] + ka * ten[qw + 1] + q * ten[qw] + kb * ten[1] + q;
+                        if (res < r)
+                            printf("%d ", res);
+                    }
+                }
+            else {
+                for (int k = 0; k < 10; k++) {
+                    if (l <= k && k <= r)
+                        printf("%d ", k);
+                }
+            }
+        }
+    }
 
-    int r=-1000000010,ans=0;
-    for (int i = 0; i < n; i++) 
-        if (r <a[i].first ) {
-            ans++;
-            r = a[i].second;
-        }else if (a[i].second < r) 
-            r = a[i].second;
-    cout << ans;
     return 0;
 }
 
