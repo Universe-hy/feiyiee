c79cb618ba739a3c9f3f9d18de26b933ddb87b68
Universe-hy <3260372408@qq.com>
2025-02-12 19:57:02 +0800

    P5022 [NOIP 2018 提高组] 旅行 ing

--- 源.cpp
+++ 源.cpp
@@ -11,54 +11,36 @@
 
 using namespace std;
 const int N = 10000010;
+int h[N], ne[N], e[N], idx=1,d[N];
 
-queue<int> a[3];
-int b[N];
+void add(int a, int b) {
+    ne[idx] = h[a], h[a] = idx, e[idx++] = b;
+    ne[idx] = h[b], h[b] = idx, e[idx++] = a;
+    d[a]++; d[b]++;
+}
 
-int maxa() {
-    int t=0;
-    for (int i = 0; i < 3; i++) 
-        if (a[i].size()) {
-            t = i;
-            break;
-        }
-    for (int i = t+1; i < 3; i++) 
-        if (a[i].size() && a[i].front() > a[t].front())
-            t = i;
-    int res = a[t].front();
-    a[t].pop();
-    return res;
 
+void dfs(int a) {
+    int id = h[a];
+    d[a] = 0;
+    printf("%d ", a);
+    while (id) {
+        if (d[e[id]])
+            dfs(e[id]);
+        id = ne[id];
+    }
 }
 int main()
 {
-    int n,m,q,u,v,t;
-    scanf("%d %d %d %d %d %d", &n, &m, &q, &u, &v, &t);
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &b[i]);
+    int n, m;
+    scanf("%d %d", &n, &m);
+    for (int i = 0; i < m; i++)
+    {
+        int a, b;
+        scanf("%d %d", &a, &b);
+        add(a, b);
     }
-    sort(b, b + n + m, greater <int>() );
-    for (int i = 0; i < n; i++) {
-        a[0].push(b[i]);
-    }
-    int rise = 0;
-    for (int i = 1; i <= m; i++) {
-        int x = maxa()+rise;
-        long long int xa = (long long)x * u / v;
-        int xb = x - xa;
-        if (i % t == 0) {
-            printf("%d ", x);
-        }
-        rise += q;
-        a[1].push(xa - rise);
-        a[2].push(xb - rise);
-    }
-    printf("\n");
-    for (int i = 0; i < n + m; i ++) {
-        int k = maxa()+rise;
-        if ((i + 1) % t == 0)
-            printf("%d ", k);
-    }
-    return 0;
+
+    dfs(1);
 }
 
