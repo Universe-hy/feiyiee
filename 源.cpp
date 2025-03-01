1f1b24f5518222d8af0b4facc1545870d0de303b
Universe-hy <3260372408@qq.com>
2025-03-01 20:45:01 +0800

    AcWing 6134. 哞叫时间II

--- 源.cpp
+++ 源.cpp
@@ -11,24 +11,34 @@
 using namespace std;
 typedef long long int ll; 
 
-const int  N=20010;
+const int  N=1000010;
 ll a[N],n;
-
+int ai[N];
+int num[N];
+bool fa[N], f[N];
 int main()
 {  
-    int T;
-    scanf("%d", &T);
-    while(T--){
-        scanf("%lld", &n);
-        ll all = 0;
-        for (int i = 1; i <= n; i++) {
-            scanf("%lld", &a[i]);
-            a[i] += a[i-1];
-        } 
-        ll ai = a[n];
-        for (int i = 0; i  <= (n - 1) / 2; i++) {
-            ai = min(ai, a[n / 2 + 1 + i] - a[i]);
+    scanf("%lld", &n);
+    
+    for (int i = 1; i <= n; i++) {
+        scanf("%lld", &a[i]);
+        num[i] = num[i - 1];
+        if (!f[a[i]])
+        {
+            f[a[i]] = 1;
+            num[i]++;
         }
-        printf("%lld %lld", ai, a[n] - ai);
+    } 
+    ll ans = 0;
+    for (int i = n; i > 0; i--) {
+        ai[a[i]]++;
+        if (ai[a[i]] == 2 && !fa[a[i]]) {
+            fa[a[i]] = 1;
+            if (num[i] == num[i - 1])
+                ans += num[i - 1] - 1;
+            else
+                ans += num[i - 1];
+        }
     }
+    printf("%lld", ans);
 }
