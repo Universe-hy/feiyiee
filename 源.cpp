065ee958616a2d8a974975e7388e29c7807ad1dc
Universe-hy <3260372408@qq.com>
2025-04-04 15:15:20 +0800

    AcWing 5437. 拐杖糖盛宴 AC

--- 源.cpp
+++ 源.cpp
@@ -1,7 +1,4 @@
-#define _CRT_SECURE_NO_WARNINGS 1
-#pragma warning(disable:6031)
-
-#include <windows.h>
+//#include<stdio.h>
 #include<iostream>
 #include<algorithm>
 #include <stdlib.h>
@@ -9,98 +6,50 @@
 #include<map>
 #include<queue>
 using namespace std;
-typedef long long int ll;
-
-const int  N = 100010;
-int ans[105][1<<20];
-int ansma[105][1 << 20];
-int ma[205];
+typedef long long ll;
+const int N = 200010;
 int n, m;
-int cheak(int x, int y) {
-    int q1 = x >> m;
-    int q2 = x - (q1 << m);
-    int q3 = y >> m;
-    int q4 = y - (q3 << m);
-    return ((q4 & (q3 | q2)) | (q3 & (q1 | q2))) ? 0 : 1; //((q3 & q2 & q1 | (q4 & q3 & q2)));
+ll a[N], b[N];
+int ai[N], idx = 1;
+void cut() {
+	int nidx = 1;
+	for (int i = 1; i < idx; i++) {
+		if (a[ai[i]] > a[ai[nidx - 1]]) {
+			ai[nidx++] = ai[i];
+		}
+	}
+	idx = nidx;
 }
 
-int cheak(int q) {
-    return (q & ((q >> 1) | (q >> 2)));
-}
-
-
-void print(int i,int w) {
-    if (i < 0)return;
-    print(i - 1, ansma[i][w]);
-    for (int q = m*2-1; q >=0; q--) {
-        if ((w >> q) & 1) {
-            cout << '*';
-        }
-        else {
-            if ((ma[i] >> q) & 1) {
-                cout << 'H';
-            }
-            else {
-                cout << 'P';
-            }
-        }
-        if (q % m ==0)
-            cout << endl;
-    }
-}
 int main() {
-    cin >> n >> m;
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+	idx = 1;
+	for (int i = 1; i < n; i++) {
+		if (a[i] > a[ai[idx - 1]]) {
+			ai[idx++] = i;
+		}
+	}
+	for (int i = 0; i < m; i++)
+		cin >> b[i];
 
-    //ma ≥ı ºªØ
-    string str;
-    for (int i = 0; i < n; i++) {
-        cin >> str;
-        for (int q = 0; q < m; q++) {
-            ma[i/2] <<= 1;
-            ma[i/2] += (str[q] == 'H');
-        }
-    }
-    if (n & 1)
-        ma[n / 2] = (ma[n / 2] << m) + (1 << m) - 1;
-    ma[n / 2 + 1] = (1 << (2 * m)) - 1;
-
-
-    for (int i = 0; i < (n + 1)/2; i++) {
-        for (int q = 0; q < (1 << (m * 2)); q++) {
-            int q1 = q >> m, q2 = q - (q1 << m);
-            //ºÏ—Èq 
-            if ((q1 & q2) | cheak(q1) | cheak(q2) | (q & ma[i]))
-                continue;
-            if(i)
-            {
-                int maxn = 0;
-                for (int w = 0; w < (1 << (m * 2)); w++) {
-                    if (cheak(w, q)) {
-                        if (maxn<ans[i - 1][w])
-                        {
-                            maxn = ans[i - 1][w];
-                            ansma[i][q] = w;
-                        }
-                    }
-                }
-                ans[i][q] += maxn;
-            }
-            for (int w = q; w; w >>= 1) {
-                ans[i][q] += (w & 1);
-            }
-        }
-    }
-    int ans1 = 0;
-    int anma = 0;
-    for (int i = 0; i < (1 << (m * 2)); i++) {
-        if (ans1 < ans[(n + 1) / 2 - 1][i])
-        {
-            ans1 = ans[(n + 1) / 2 - 1][i];
-            anma = i;
-        }
-    }
-    cout << ans1<<endl;
-    print((n + 1) / 2 - 1,anma);
-    return 0;
+	for (int i = 0; i < m; i++) {
+		ll down = 0;
+		for (int q = 0; q < idx; q++) {
+			int top = min(a[ai[q]], b[i]);
+			if (top > down)
+			{
+				a[ai[q]] += top - down;
+				down = top;
+				if (down >= b[i])
+					break;
+			}
+		}
+	}
+	for (int i = 0; i < n; i++) {
+		cout << a[i] << endl;
+	}
+	return 0;
 }
-
