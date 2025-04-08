02867b64dd4ae092be6a2c6268cd28f38d8bc021
Universe-hy <3260372408@qq.com>
2025-04-08 14:36:37 +0800

    第十五届蓝桥杯大赛软件赛省赛 C/C++ 大学 A 组 五子棋对弈 AC

--- 源.cpp
+++ 源.cpp
@@ -1,45 +1,53 @@
 #include<iostream>
 #include<algorithm>
 #include <stdlib.h>
-#include<string>
-#include<math.h>
-#include<queue>
 using namespace std;
 typedef long long int ll;
 
 const int  N = 400010;
+int w5 = (1 << 5) - 1;
+int cheak(ll i) {
+	int a = 0, b = w5;
 
-int a[N], h[N], rt[N], idx;
-int main() {
-	int T, n;
-	cin >> T;
-	while (T--) {
-		cin >> n;
-		for (int i = 0; i < n; i++) {
-			cin >> h[i];
-		}
-		for (int i = 0; i < n; i++) {
-			cin >> a[i];
-		}
-		int t;
-		for (int i = 0; i < n; i++) {
-			cin >> t;
-			rt[t] = i;
-		}
-		int l = 0, r = 1e9;
-		for (int i = 0; i < n - 1; i++) {
-			double dh = h[rt[i]] - h[rt[i + 1]];
-			double da = a[rt[i + 1]] - a[rt[i]];
-			if (da < 0)
-				l = max(l, (int)floor(dh / da) + 1);
-			else if (da > 0)
-				r = min(r, (int)ceil(dh / da) - 1);
-			else if (dh <= 0) {
-				l = -1;
-				break;
-			}
-		}
-		cout << (r >= l ? l : -1) << endl;
+	int ka = 0, kb = 1, ya = 0, yb = 1;
+
+	for (int q = 0; q < 25; q += 5) {
+
+		int k = (i >> q) & w5;
+		if (k == 0 || k == w5)
+			return 0;
+		a |= k;
+		b &= k;
+
+		int x = (k >> (4 - q / 5)) & 1;
+		ka |= x;
+		kb &= x;
+
+		int y = (k >> (q / 5)) & 1;
+		ya |= y;
+		yb &= y;
 	}
+	if (a != w5 || b != 0)
+		return 0;
+	if (ka != 1 || kb != 0)
+		return 0;
+	if (ya != 1 || yb != 0)
+		return 0;
+
+	int num = 0;
+	while(i) {
+		num += i & 1;
+		i >>= 1;
+	}
+	if (num != 13)
+		return 0;
+	return 1;
+}
+int main() {
+	ll ans = 0;
+	for (ll i = 0; i < (1 << 25); i++) {
+		ans += cheak(i);
+	}
+	cout << ans;
 	return 0;
 }
