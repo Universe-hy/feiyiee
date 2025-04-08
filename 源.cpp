27bd3210ed4520412b747ef79260ed625e967fa6
Universe-hy <3260372408@qq.com>
2025-04-08 15:11:46 +0800

    第十五届蓝桥杯大赛软件赛省赛 C/C++ 大学 A 组 训练士兵 AC 注意开 long long

--- 源.cpp
+++ 源.cpp
@@ -1,53 +1,36 @@
 #include<iostream>
 #include<algorithm>
-#include <stdlib.h>
+#include<stdlib.h>
+#include<vector>
 using namespace std;
 typedef long long int ll;
 
-const int  N = 400010;
-int w5 = (1 << 5) - 1;
-int cheak(ll i) {
-	int a = 0, b = w5;
-
-	int ka = 0, kb = 1, ya = 0, yb = 1;
-
-	for (int q = 0; q < 25; q += 5) {
-
-		int k = (i >> q) & w5;
-		if (k == 0 || k == w5)
-			return 0;
-		a |= k;
-		b &= k;
-
-		int x = (k >> (4 - q / 5)) & 1;
-		ka |= x;
-		kb &= x;
-
-		int y = (k >> (q / 5)) & 1;
-		ya |= y;
-		yb &= y;
-	}
-	if (a != w5 || b != 0)
-		return 0;
-	if (ka != 1 || kb != 0)
-		return 0;
-	if (ya != 1 || yb != 0)
-		return 0;
-
-	int num = 0;
-	while(i) {
-		num += i & 1;
-		i >>= 1;
-	}
-	if (num != 13)
-		return 0;
-	return 1;
-}
+const int  N = 100010;
+ll n, S;
+typedef pair<int, int> PII;
+vector<PII> a;
 int main() {
-	ll ans = 0;
-	for (ll i = 0; i < (1 << 25); i++) {
-		ans += cheak(i);
+	cin >> n >> S;
+	int x, y;
+	for (int i = 0; i < n; i++) {
+		cin >> x >> y;
+		a.push_back(make_pair(y, x));
 	}
-	cout << ans;
+	sort(a.begin(), a.end(), greater<PII>());
+
+	int id = 0;
+	ll sum = 0;
+	for (int i = 0; i < n; i++) {
+		sum += a[i].second;
+		if (sum >= S) {
+			id = i;
+			break;
+		}
+	}
+	ll cost = a[id].first * S;
+	for (int i = 0; i < id; i++) {
+		cost += (ll)(a[i].first - a[id].first) * a[i].second;
+	}
+	cout << cost;
 	return 0;
 }
