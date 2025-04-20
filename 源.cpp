7c9f8ab3318aedd220d483eb3f7256d5f5ec1ec4
Universe-hy <3260372408@qq.com>
2025-04-20 16:47:58 +0800

    第十六届蓝桥杯省赛C++组真题 水质检测 AC

--- 源.cpp
+++ 源.cpp
@@ -1,93 +1,59 @@
 #include <iostream>
-#include <iomanip>
-#include <cmath>
-#include<vector>
+#include <cstring>
+#include <algorithm>
+
 using namespace std;
-const int M = 10;
-vector<int> w, r, ans;
-int k = 0;
-void w2() {
-	for (int i = 0; i < w.size(); i++) {
-		w[i] *= 2;
-	}
-	int id = w.size() - 1;
 
-	for (int i = 0; i < id; i++)
-	{
-		if (w[i] / M) {
-			w[i + 1] += w[i] / M;
-			w[i] %= M;
-		}
-	}
-
-	if (w[id] / M) {
-		w.push_back(w[id] / M);
-		w[id] %= M;
-	}
-
-}
-
+string s[2];
 int main()
 {
-	int n;
-	string R;
-	cin >> n;
-	cin >> R;
-	w.push_back(2);
-	for (int i = 1; i < n; i++)
-	{
-		w2();
-	}
+    cin >> s[0] >> s[1];
 
-	for (int i = R.size() - 1; i >= 0; i--) {
-		if (R[i] == '.')
-		{
-			k = R.size() - i - 1;
-			continue;
-		}
-		r.push_back(R[i] - '0');
-	}
+    int be = 0, ed = 0;
+    for (int i = 0; s[0][i]; i++)
+        if (s[0][i] == '#' || s[1][i] == '#')
+        {
+            be = i;
+            break;
+        }
+    for (int i = 0; s[0][i]; i++)
+        if (s[0][i] == '#' || s[1][i] == '#')
+            ed = i;
 
-	for (int i = 0; i < w.size(); i++) {
-		for (int q = 0; q < r.size(); q++) {
-			if (i + q >= ans.size()) {
-				ans.push_back(w[i] * r[q]);
-			}
-			else {
-				ans[i + q] += w[i] * r[q];
-			}
+    int f0 = (s[0][be] == '#');
+    int f1 = (s[1][be] == '#');
+    int f2 = 0;
+    int ans = 0;
 
-			for (int w = i + q; ans[w] / M; w++) {
 
-				if (w + 1 < ans.size()) {
-					ans[w + 1] += ans[w] / M;
-				}
-				else {
-					ans.push_back(ans[w] / M);
-				}
-				ans[w] %= M;
-			}
-		}
-	}
+    for (int i = be + 1; i <= ed; i++)
+    {
+        int k0 = (s[0][i] == '#'), k1 = (s[1][i] == '#');
+        if ((f0 && k0) || (f1 && k1) || (f2 && (k0 || k1)))
+        {
+            f0 = k0;
+            f1 = k1;
+            f2 = 0;
+        }
+        else
+        {
+            ans++;
+            if (f0 == f1)
+            {
+                f0 = k0;
+                f1 = k1;
+                f2 = 1;
+            }
+            else
+            {
+                f0 = f0 || k0;
+                f1 = f1 || k1;
+                f2 = 0;
+            }
+        }
+    }
+    cout << ans;
 
-	if (ans[k - 1] >= 5) {
-		ans[k]++;
-
-		for (int w = k; ans[w] / M; w++) {
-
-			if (w + 1 < ans.size()) {
-				ans[w + 1] += ans[w] / M;
-			}
-			else {
-				ans.push_back(ans[w] / M);
-			}
-			ans[w] %= M;
-		}
-	}
-
-	for (int i = ans.size() - 1; i >= k; i--) {
-		cout << ans[i];
-	}
-
-	return 0;
 }
+
+
