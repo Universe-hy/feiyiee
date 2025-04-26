1004f153b2572b6d88aff5b3316d6aa949728896
Universe-hy <3260372408@qq.com>
2025-04-26 19:57:36 +0800

    洛谷  P1364 医院设置  AC

--- 源.cpp
+++ 源.cpp
@@ -1,40 +1,47 @@
-#include<bits/stdc++.h>
+
 #include<iostream>
+#include<string>
 #include<algorithm>
 #include <stdlib.h>
+#include <set>
 #include <queue>
 using namespace std;
-typedef long long ll;
-typedef pair<int, int> PII;
-const int N = 100010;
-int a[n];
 
-int main()
+typedef long long ll;
+const int N = 10001;
+
+struct tree {
+    int r, l;
+    ll n,cout;
+    bool operator <(const tree& v) {
+        return n < v.n;
+    }
+} a[N];
+
+ll ans;
+
+ll count(int x,int d) {
+    ans += a[x].n * d;
+    if (a[x].r)a[x].cout += count(a[x].r, d + 1);
+    if (a[x].l)a[x].cout += count(a[x].l, d + 1);
+    return a[x].cout += a[x].n;
+}
+
+void count(int x,ll res, ll top) {
+    ans = min(ans, res);
+    top += a[x].n;
+    if (a[x].r) count(a[x].r, res - a[a[x].r].cout + a[a[x].l].cout + top, top + a[a[x].l].cout);
+    if (a[x].l) count(a[x].l, res + a[a[x].r].cout - a[a[x].l].cout + top, top + a[a[x].r].cout);
+}
+
+
+signed main()
 {
-	int n, k;
-	cin >> n;
-	for (int i = 1; i <= n; i++)
-	{
-		cin >> k;
-		a[k]++;
-	}
-	for (int i = 100000; i > 0; i--)
-	{
-		int num = 0;
-		for (ll q = i; q <= 100000; q *= i)
-		{
-			num += a[q];
-		}
-		if (num >= 3)
-		{
-			for (ll q = i; q <= 100000; q *= i)
-			{
-				if (a[q]) {
-					printf("%d ", q);
-				}
-			}
-			break;
-		}
-	}
-	return 0;
+    int T;
+    cin >> T;
+    for(int i= 1;i<=T;i++) cin >> a[i].n >> a[i].r >> a[i].l;
+    count(1, 0);
+    count(1,ans, (ll)0);
+    cout << ans;
+    return 0;
 }
